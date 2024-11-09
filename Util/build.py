acdc3dc4d55d092aa0545fa8c2f82df8c5eac7a3
iNanoToxin <dylanleodaniel@gmail.com>
2024-11-09 15:30:26 -0500

    Updated build.py and clean.py

--- Util/build.py
+++ Util/build.py
@@ -12,20 +12,17 @@ assert os.system("npm install") == 0, "failed to run `npm install`"
 assert os.system("npm run build") == 0, "failed to run `npm run build`"
 
 
-build_file = "build.zip"
 zip_files = ["main.js", "styles.css", "manifest.json"]
 
 
 existing_files = list(filter(lambda f: os.path.isfile(f), zip_files))
-project_name = os.path.basename(sys.argv[1])
+build_file = f"{os.path.basename(sys.argv[1])}.zip"
 
 if len(existing_files) > 0:
     # Create a zip file and add each file
     with zipfile.ZipFile(build_file, "w") as zipf:
-        zipf.mkdir(project_name)
-
         for file in existing_files:
-            zipf.write(file, os.path.join(project_name, file))
+            zipf.write(file)
             print(f"Added {file} to {build_file}")
 
     print("Zipping complete.")
